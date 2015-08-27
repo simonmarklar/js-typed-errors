a56520b5255a1cfe826e857c421dba8ec4fa04bb
simonmarklar <simondobie23@gmail.com>
2015-08-27 12:16:41 +1000

    add missing build files.

--- dist/typed-errors.js
+++ dist/typed-errors.js
@@ -40,7 +40,7 @@ module.exports = makeErrorType;
 var makeErrorType = require('../make-error-type.js');
 
 function ActionAbortedError(msg){
-  this.msg = msg || 'The action was aborted';
+  this.message = msg || 'The action was aborted';
 }
 
 
@@ -52,8 +52,14 @@ module.exports = makeErrorType( 'ActionAborted', ActionAbortedError );
 
 var makeErrorType = require('../make-error-type.js');
 
+/**
+ * @param {string} [msg='NotFound'] A Message to display
+ *
+ * @property {string} [message='NotFound'] - The error message
+ * @property {string} [name='NotFound'] - The name of the error
+ */
 function NotFoundError(msg){
-  this.msg = msg || 'The specified url was not found';
+  this.message = msg || 'The specified url was not found';
 }
 
 
@@ -69,7 +75,7 @@ var makeErrorType = require('../make-error-type.js');
 //see http://stackoverflow.com/questions/783818/how-do-i-create-a-custom-error-in-javascript
 
 /**
- * @param {string} A Message to display
+ * @param {string} [msg='Not Implemented'] A Message to display
  *
  * @property {string} [message='Not Implemented'] - The error message
  * @property {string} [name='NotImplemented'] - The name of the error
